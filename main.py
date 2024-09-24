1e16f32099feed841d1d17cf18f2d3122ec062af
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-09-24 20:47:01 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-harf = input('harf kiriting: ') *5
-print(harf)
 
-number1 = int(input('Son kiritng: '))
-number2 = int(input('Son kiritng: '))
+ism = input("Ismingizni kiriting: ")
+print(ism*5) 
 
-print(f'{number1} - {number2} = {number1 - number2}')
-print(f'{number1} + {number2} = {number1 + number2}')
-print(f'{number1} / {number2} = {number1 / number2}')
-print(f'{number1} * {number2} = {number1 * number2}')
-print(f'{number1} // {number2} = {number1 // number2}')
-print(f'{number1} ** {number2} = {number1 ** number2}')
 
-number3 = int(input('Katta son kiritng: '))
-number4  = int(input('Kichik son kiritng: '))
+son1 = int(input("Birinchi sonni kiriting: "))
+son2 = int(input("Ikkinchi sonni kiriting: "))
 
-bolish = number3 / number4
 
-print(f"Ceil:{bolish.__ceil__()}")
-print(f"Floor:{bolish.__floor__()}")
+print(f'Yigindisi {son1 + son2}')
+print(f'Ayirmasi {son1 - son2}')
+print(f'Kopaytirmasi {son1 * son2}')
+print(f'Bolinishi {son1 / son2}')
+print(f'Darajaga kopaytirish {son1 ** son2}')
+print(f'Butun qismi {son1 // son2}')
+
+
+
+son = float(input("Son kiriting: "))
+print("Sonning 3-darajasi:", son ** 3)
+
+import math
+
+son = int(input("Son kiriting: "))
+
+print("Darajaga oshirish:", math.pow(son, 2))
+print("Kvadrat ildizi:", math.sqrt(son))
+print("Absolyut qiymati:", abs(son))
+
+
+
+
+
 
 
 
